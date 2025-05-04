26aeb506a57f4d0bd7cd711c7d9a9593d78dbf6b
powlo <paulcbackhouse@gmail.com>
2025-05-04 15:58:46 +0100

    Add client models.

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    company = models.ForeignKey("clients.Company", on_delete=models.CASCADE)
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
