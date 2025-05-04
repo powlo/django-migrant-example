c9ecfbad227d7430f3cb462ad960a6234536923d
powlo <paulcbackhouse@gmail.com>
2025-05-04 15:13:39 +0100

    Tweak question length.

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField("date published")
 
 
